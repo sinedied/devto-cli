1e18ed359011c75247af6f43672f547132aa5ab5
sinedied <noda@free.fr>
2021-10-05 18:59:02 +0200

    test: fix windows test (maybe)

--- test/articles.spec.ts
+++ test/articles.spec.ts
@@ -1,19 +1,27 @@
-import path, { dirname } from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
 const { getArticlesFromFiles } = await import('../src/article');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const resetCwd = () => {
+  process.chdir(join(__dirname, '..'));
+};
+
 describe('articles', () => {
+  beforeEach(resetCwd);
+  afterEach(resetCwd);
+
   describe('getArticlesFromFiles', () => {
     it('should get articles from given glob', async () => {
-      const articles = await getArticlesFromFiles([path.posix.join(__dirname, 'fixtures/*.md')]);
+      process.chdir('test');
+      const articles = await getArticlesFromFiles(['fixtures/*.md']);
       expect(articles.length).toBe(1);
       expect(articles[0].data.title).toBe('this is an article');
     });
 
     it('should return an empty array', async () => {
-      const articles = await getArticlesFromFiles([path.posix.join(__dirname, '*.md')]);
+      const articles = await getArticlesFromFiles(['*.md']);
       expect(articles).toEqual([]);
     });
   });
